6b42557612dbcb2b7f61f226942aa676ab9cb1de
ganeshmani <ganeshmani009@gmail.com>
2024-06-04 10:28:38 +0530

    fix: change action

--- nodejs-aws-ec2/app/index.js
+++ nodejs-aws-ec2/app/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!! Welcome to NodeJS AWS EC2 Instance!!");
+  res.send("Hello World!!!Welcome to NodeJS AWS EC2 Instance!!");
 });
 
 const PORT = process.env.PORT || 4500;
